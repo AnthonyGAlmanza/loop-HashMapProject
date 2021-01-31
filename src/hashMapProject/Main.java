6b3b58ddc0b2e06617827257ba1a6003f3bebc5d
AnthonyGAlmanza <antman_2012@yahoo.com>
2021-01-31 16:52:19 -0600

    Update comment out / closed second scanner

--- src/hashMapProject/Main.java
+++ src/hashMapProject/Main.java
@@ -8,63 +8,63 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-//		Scanner userInput = new Scanner(System.in);
-//		ArrayList<Integer> numberList = new ArrayList<Integer>();
-//		ArrayList<String> questionList = new ArrayList<String>();
-//		questionList.add("Type a number");
-//		questionList.add("Type a second number");
-//		questionList.add("Type a third number");
-//		questionList.add("Type a fourth number");
-//		questionList.add("Type a fifth number");
-//		
-//		
-//		for(int i = 0; i < questionList.size(); i++) {
-//			System.out.println(questionList.get(i));
-//			numberList.add(userInput.nextInt());
-//		}
-//		userInput.close();
-//		
-//		System.out.println(numberList);
-//		
-//		int sum = 0;
-//		
-//		for(int i = 0; i < numberList.size(); i++) {
-//			sum += numberList.get(i);
-//		}
-//		
-//		System.out.printf("The sum of these numbers is: %d \n", sum);
-//
-//		
-//		int product = 1;
-//		
-//		for(int i = 0; i < numberList.size(); i++) {
-//			product *= numberList.get(i);
-//		}
-//
-//		System.out.printf("The product of these numbers is: %d \n", product);
-//		
-//		
-//		
-//		int largest = 0;
-//		
-//		for(int i = 0; i < numberList.size(); i++) {
-//			if(largest < numberList.get(i)) {
-//			largest = numberList.get(i);
-//		}
-//		}
-//		System.out.printf("The largest of these numbers is: %d \n", largest);
-//		
-//		
-//		int smallest = numberList.get(0);
-//		
-//		for(int j = 0; j < numberList.size(); j++) {
-//			if(smallest > numberList.get(j)) {
-//				smallest = numberList.get(j);
-//			}
-//			
-//		}
-//		
-//		System.out.printf("The smallest of these numbers is: %d \n", smallest);
+		Scanner userInput = new Scanner(System.in);
+		ArrayList<Integer> numberList = new ArrayList<Integer>();
+		ArrayList<String> questionList = new ArrayList<String>();
+		questionList.add("Type a number");
+		questionList.add("Type a second number");
+		questionList.add("Type a third number");
+		questionList.add("Type a fourth number");
+		questionList.add("Type a fifth number");
+		
+		
+		for(int i = 0; i < questionList.size(); i++) {
+			System.out.println(questionList.get(i));
+			numberList.add(userInput.nextInt());
+		}
+		userInput.close();
+		
+		System.out.println(numberList);
+		
+		int sum = 0;
+		
+		for(int i = 0; i < numberList.size(); i++) {
+			sum += numberList.get(i);
+		}
+		
+		System.out.printf("The sum of these numbers is: %d \n", sum);
+
+		
+		int product = 1;
+		
+		for(int i = 0; i < numberList.size(); i++) {
+			product *= numberList.get(i);
+		}
+
+		System.out.printf("The product of these numbers is: %d \n", product);
+		
+		
+		
+		int largest = 0;
+		
+		for(int i = 0; i < numberList.size(); i++) {
+			if(largest < numberList.get(i)) {
+			largest = numberList.get(i);
+		}
+		}
+		System.out.printf("The largest of these numbers is: %d \n", largest);
+		
+		
+		int smallest = numberList.get(0);
+		
+		for(int j = 0; j < numberList.size(); j++) {
+			if(smallest > numberList.get(j)) {
+				smallest = numberList.get(j);
+			}
+			
+		}
+		
+		System.out.printf("The smallest of these numbers is: %d \n", smallest);
 		
 		
 		
@@ -94,7 +94,7 @@ public class Main {
 			System.out.println("Oh we don't got any of those 'round here.");
 		}
 		
-
+		inputModel.close();
 
 		
 		
